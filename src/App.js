d7f4186b7833d6729cd93406c73335e94381098d
alsgurdl <fdf98952@gmail.com>
2024-05-02 18:20:35 +0900

    5/2-18

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from './components/Expense/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
@@ -7,32 +7,51 @@ function App() {
   //지출 항목 객체 배열
   const expenses = [
     {
+      id: 1,
       title: '바나나',
       price: 3000,
-      date: new Date(2023, 3 - 1, 23),
+      date: new Date(2022, 3 - 1, 23),
     },
     {
+      id: 2,
       title: 'BBQ치킨',
       price: 20000,
       date: new Date(2023, 5 - 1, 21),
     },
     {
+      id: 3,
       title: '도미노피자',
       price: 35000,
-      date: new Date(2023, 7 - 1, 4),
+      date: new Date(2024, 7 - 1, 4),
+    },
+    {
+      id: 4,
+      title: '엽기떡붂이',
+      price: 18000,
+      date: new Date(2023, 5 - 1, 1),
     },
   ];
+  //지출 객체배열을 상태변수로 관리
+  const [expenseList, setExpenseList] = useState(expenses);
 
   // ExpenseForm에게 내려보낼 함수
   const addExpenseHandler = (newExpense) => {
     console.log('App 컴포넌트에서 응답함!');
     console.log('newExpense: ', newExpense);
+
+    const modfyExpense = {
+      ...newExpense,
+      id: expenseList[expenseList.length - 1].id + 1,
+    };
+    console.log(modfyExpense);
+
+    setExpenseList([...expenseList, modfyExpense]);
   };
 
   return (
     <>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <Expenses items={expenseList} />
     </>
   );
 }
